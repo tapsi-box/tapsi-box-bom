35cebcabdb45998a4c8865083e2b1638e6c066de
Shahryar Safizadeh <shahryar.safizadeh@tapsi.cab>
2025-09-17 18:27:53 +0330

    update dependencies: bump metrics-core to 1.0.3 and utilities-starter to 0.9.2

--- build.gradle.kts
+++ build.gradle.kts
@@ -14,8 +14,8 @@ dependencies {
         api("cab.tapsi.libs.map-zones:tapsi-mapzones:3.0.0")
         api("cab.tapsi.libs.kafka:kafka-producer-starter:3.3.0")
         api("io.github.mahdibohloul:projectreactor-retry-aop:2.0.0-RC2")
-        api("box.tapsi.libs:metrics-core:1.0.2")
-        api("box.tapsi.libs:utilities-starter:0.9.1")
+        api("box.tapsi.libs:metrics-core:1.0.3")
+        api("box.tapsi.libs:utilities-starter:0.9.2")
         api("box.tapsi.libs:scheduler-starter:0.9.0")
     }
 }
