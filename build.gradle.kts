58397586d66743610ecd0cad5c4948582c2fc5a1
mahdibohloul <mahdi.bohloul@tapsi.cab>
2025-11-30 18:22:53 +0330

    Update project version to 1.0.6 and bump statemachine dependency to 0.10.0

--- build.gradle.kts
+++ build.gradle.kts
@@ -4,7 +4,7 @@ plugins {
 }
 
 group = "box.tapsi.libs"
-version = "1.0.5"
+version = "1.0.6"
 
 dependencies {
   constraints {
@@ -14,7 +14,7 @@ dependencies {
     api("cab.tapsi.libs:localization-spring-boot-starter:0.0.2")
     api("cab.tapsi.libs.map-zones:tapsi-mapzones:3.0.0")
     api("io.github.mahdibohloul:projectreactor-retry-aop:2.0.0-RC2")
-    api("io.github.mahdibohloul:statemachine:0.9.0")
+    api("io.github.mahdibohloul:statemachine:0.10.0")
     api("io.github.mahdibohloul:spring-setting-core:0.9.1")
     api("io.github.mahdibohloul:spring-setting-memory:0.9.1")
     api("io.github.mahdibohloul:spring-setting-mongodb:0.9.1")
