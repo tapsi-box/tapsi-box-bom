d736682b397f3d489421f202aed24074b21fbe82
mahdibohloul <mahdi.bohloul@tapsi.cab>
2026-08-15 14:27:56 +0330

    chore: bump spring-reactor-kafka to 1.0.0 and projectreactor-retry-aop to 2.0.0, release 2.0.0-RC2

--- build.gradle.kts
+++ build.gradle.kts
@@ -4,16 +4,16 @@ plugins {
 }
 
 group = "box.tapsi.libs"
-version = "2.0.0-RC1"
+version = "2.0.0-RC2"
 
 dependencies {
   constraints {
     api("io.github.mahdibohloul:kpring-mediatr-starter:2.0.2")
-    api("io.github.mahdibohloul:spring-reactor-kafka:1.0.0-RC1")
+    api("io.github.mahdibohloul:spring-reactor-kafka:1.0.0")
     api("cab.tapsi.libs.loggingstarter:tapsi-logging-starter:3.0.0")
     api("cab.tapsi.libs:localization-spring-boot-starter:0.0.2")
     api("cab.tapsi.libs.map-zones:tapsi-mapzones:3.0.0")
-    api("io.github.mahdibohloul:projectreactor-retry-aop:2.0.0-RC2")
+    api("io.github.mahdibohloul:projectreactor-retry-aop:2.0.0")
     api("io.github.mahdibohloul:statemachine:0.11.0")
     api("io.github.mahdibohloul:spring-setting-core:1.0.0")
     api("io.github.mahdibohloul:spring-setting-memory:1.0.0")
